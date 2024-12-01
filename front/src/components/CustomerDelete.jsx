1948a169434d7b83eea7a1b6a8db841a9c524f78
donkim <donkim@gmail.com>
2024-12-01 23:20:28 +0900

    삭제 경고 다이얼로그

--- front/src/components/CustomerDelete.jsx
+++ front/src/components/CustomerDelete.jsx
@@ -1,8 +1,10 @@
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useState } from 'react';
 
 const CustomerDelete = (props) => {
     const [id, setId] = useState(props.id);
+    const [isOpen, setIsOpen] = useState(false);
 
      const deleteCustomer = async() =>{
         const url = 'http://localhost:5000/api/customers/' + id;
@@ -16,9 +18,31 @@ const CustomerDelete = (props) => {
         }
      };
 
+     const handleClickOpen = () => {
+        setIsOpen(true);
+     };
+
+     const handleClose = () => {
+        setIsOpen(false);
+     };
+
     return (
         <>
-            <button onClick={()=>{console.log('삭제'); deleteCustomer(id);}}>삭제</button>
+            <Button variant="contained" color="secondary" onClick={()=>{handleClickOpen();}}>삭제</Button>
+            <Dialog onClose={handleClose} open={isOpen}>
+                <DialogTitle>
+
+                </DialogTitle>
+                <DialogContent>
+                    <Typography gutterBottom>
+                        선택한 고객정보가 삭제됩니다.
+                    </Typography>
+                </DialogContent>
+                <DialogActions>
+                    <Button variant='contained' color="primary" onClick={(e)=>{deleteCustomer(id);}}>삭제</Button>
+                    <Button variant='contained' color="primary" onClick={(e)=>{handleClose(id);}}>닫기</Button>
+                </DialogActions>
+            </Dialog>
         </>
     );
 };
